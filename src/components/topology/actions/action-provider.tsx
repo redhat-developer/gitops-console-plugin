452cc7c974ed2320cc66cf4a6c30080aeccb5059
Keith Chong <kykchong@redhat.com>
2025-07-22 15:10:53 -0400

    feat: Rollouts: Implement Delete Action (#7127)
    Signed-off-by: Keith Chong <kykchong@redhat.com>

--- src/components/topology/actions/action-provider.tsx
+++ src/components/topology/actions/action-provider.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 
-import { Action } from '@openshift-console/dynamic-plugin-sdk';
+import { Action, useDeleteModal } from '@openshift-console/dynamic-plugin-sdk';
 import { getResource } from '@openshift-console/dynamic-plugin-sdk-internal';
 import { GraphElement, isGraph } from '@patternfly/react-topology';
 
-import { editRollout } from './creators';
+import { editRollout, getDeleteRolloutAction } from './creators';
 
 export const useGitOpsActionProviderForTopology = (element: GraphElement) => {
+  const resource = getResource(element);
+  const deleteModal = useDeleteModal(resource);
+
   const actions = React.useMemo(() => {
-    const resource = getResource(element);
     if (!resource) {
       return [];
     }
@@ -17,10 +19,10 @@ export const useGitOpsActionProviderForTopology = (element: GraphElement) => {
     }
     const addActions: Action[] = [];
     addActions.push(editRollout(resource));
-    // addActions.push(getDeleteRolloutAction(resource)) // Add Delete action later
+    addActions.push(getDeleteRolloutAction(deleteModal));
     // Look at console/frontend/packages/dev-console/src/actions/add-resources.tsx : disabledActionsFilter
     return addActions;
-  }, [element]);
+  }, [element, resource, deleteModal]);
   return [actions, true, undefined];
 };
 const TYPE_APPLICATION_GROUP = 'part-of';
