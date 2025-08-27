4a2ea40e2e0db65aa686fcc6d06a885e1108a7bc
Atif Ali <atali@redhat.com>
2025-08-27 15:07:31 -0400

    Add on AppSetListPage Favourite Icon and Badge
    Signed-off-by: Atif Ali <atali@redhat.com>

--- src/gitops/components/shared/ApplicationSetList.tsx
+++ src/gitops/components/shared/ApplicationSetList.tsx
@@ -31,6 +31,7 @@ import { ApplicationSetKind, ApplicationSetModel } from '../../models/Applicatio
 import { getAppSetStatus, getAppSetGeneratorCount } from '../../utils/gitops';
 import ActionsDropdown from '../../utils/components/ActionDropDown/ActionDropDown';
 import { modelToGroupVersionKind, modelToRef } from '../../utils/utils';
+import DevPreviewBadge from '../../../components/import/badges/DevPreviewBadge';
 
 // Import status icons for consistency with ApplicationList
 import {
@@ -188,7 +189,11 @@ const ApplicationSetList: React.FC<ApplicationSetProps> = ({
   return (
     <div>
       {showTitle == undefined && (
-        <ListPageHeader title={t('plugin__gitops-plugin~ApplicationSets')}>
+        <ListPageHeader
+          title={t('plugin__gitops-plugin~ApplicationSets')}
+          badge={<DevPreviewBadge />}
+          hideFavoriteButton={false}
+        >
           <ListPageCreate groupVersionKind={modelToRef(ApplicationSetModel)}>
             Create ApplicationSet
           </ListPageCreate>
